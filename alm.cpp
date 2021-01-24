76198c9d77cdfaa24abde72a8ea9ff2ffd0c8468
unknown989 <55900066+unknown989@users.noreply.github.com>
2021-01-24 17:54:16 +0000

    Update alm.cpp

--- alm.cpp
+++ alm.cpp
@@ -1,42 +1,44 @@
 #include <iostream>
 #include <fstream>
-#include <ncurses.h>
 
+#define VERSION "Version : 1.0"
 
 using namespace std;
 
-string readFile(string filename);
+void readFile(string filename);
 
 int main(int argc,char* argv[]){
-
-	if(!argv[1]){
-		cout << "No file Included" << endl;
-		return 1;
-	}
-	string data = readFile(argv[1]);
-	cout << data << endl;
-	return 0;
+        std::string arg = argv[1];
+        if(!arg.c_str()){
+                cout << "No file Included" << endl;
+                return 1;
+        }
+        if(arg == "-h" || arg == "--help"){
+                cout << "Usage : alm <filename>\nHelp: alm <-h><--help>\nVersion: alm -v" << endl;
+                return 0;
+        }else if(arg == "-v"){
+                cout << VERSION << endl;
+                return 0;
+        }else{
+                readFile(arg);
+        }
+        return 0;
 
 }
 
+void readFile(string filename){
+        string data;
+        string line;
+        fstream file;
 
+        file.open(filename,fstream::in);
 
-string readFile(string filename){
-	
-	string data;
-	string line;
-	fstream file;
-
-	file.open(filename,fstream::in);
-
-	if(!file.is_open()){
-		cerr << "Error While Opening The File" << endl;
-		exit(1);
-	}else{
-		while(getline(file,line)){data.append(line+"\n");};
-		file.close();
-	}
-
-	return data;
+        if(!file.is_open()){
+                cerr << "Error While Opening The File" << endl;
+                exit(1);
+        }else{
+                while(getline(file,line)){cout << line << endl;};
+                file.close();
+        }
 
 }
